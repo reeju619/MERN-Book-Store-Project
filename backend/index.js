4afa02ffae8253e83cbef3cd06f168e80c7a6749
reeju619 <87873410+reeju619@users.noreply.github.com>
2023-09-30 15:04:32 +0530

    Second Commit

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import open from 'open';
 import mongoose from "mongoose";
-import {Book} from "./models/bookModel.js";
+import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -12,52 +13,27 @@ const port = process.env.PORT || 9000;
 // Middleware for parsing request body
 app.use(express.json());
 
+// Middleware for handling CORS POLICY
+// Option 1: Allow All Origins with Default of cors(*)
+app.use(cors());
+// Option 2: Allow Custom Origins
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+//   })
+// );
+
 
 app.get('/', (request, response) => {
     console.log(request);
     return response.status(234).send('Hello World!');
 });
-// Route for Save a new Book
-app.post('/books', async (request, response) => {
-    try {
-        if (
-            !request.body.title ||
-            !request.body.author ||
-            !request.body.publishYear
-        ) {
-            return response.status(400).send({
-                message: 'Send all required fields: title, author, publishedYear',
-            });
-        }
-        const newBook = {
-            title: request.body.title,
-            author: request.body.author,
-            publishYear: request.body.publishYear,
-        };
-        const books = await Book.create(newBook);
 
-        return response.status(200).json({
-            count: books.length,
-            data: books,
-        });
-    }
-     catch (error) {
-        console.log(error.message);
-        response.status(500).send({ message: error.message });
-    }
-});
+app.use('/books', booksRoute);
 
-// Route for Get All Books from database
-app.get('/books', async (request, response) => {
-    try {
-        const books = await Book.find({});
-        return response.status(200).json(books);
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({ message: error.message })
-    }
-});
-
+// Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
 }).then(() => {
     console.log("App connected to database.");
